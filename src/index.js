d2787081274469fc96aff11f1b937760e29ecae7
zolandevk <49051873+zolandevk@users.noreply.github.com>
2021-09-04 19:14:54 +0700

    projek dah kelar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import CardList from "./components/CardList";
+import Quotes from "./components/Quotes";
+import Gambar from "./assets/images/photo.jpg";
+const quotes =
+"Disaat payung teduh ku dengar , disitu lah tugas-tugas terbabat abis";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+<React.StrictMode>
+<App />
+<CardList
+name="zolan"
+nim="21120118130048"
+kelompok="01"
+isNameBold
+image={Gambar}
+/>
+<Quotes author="Orang" quotes={quotes} />
+</React.StrictMode>,
+document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
